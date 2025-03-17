2621908c4d80168e9bee9d8bf5adbf185af4338e
Harsh Rajput <101476748+Harshalex@users.noreply.github.com>
2025-03-17 13:15:25 +0530

    Update CartPage.jsx

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import { useCartStore } from "../stores/useCartStore";
-import { motion } from "framer-motion";
 import { ShoppingCart } from "lucide-react";
 import CartItem from "../components/CartItem";
 import PeopleAlsoBought from "../components/PeopleAlsoBought";
@@ -14,12 +13,7 @@ const CartPage = () => {
 		<div className='py-8 md:py-16'>
 			<div className='mx-auto max-w-screen-xl px-4 2xl:px-0'>
 				<div className='mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8'>
-					<motion.div
-						className='mx-auto w-full flex-none lg:max-w-2xl xl:max-w-4xl'
-						initial={{ opacity: 0, x: -20 }}
-						animate={{ opacity: 1, x: 0 }}
-						transition={{ duration: 0.5, delay: 0.2 }}
-					>
+					<div className='mx-auto w-full flex-none lg:max-w-2xl xl:max-w-4xl'>
 						{cart.length === 0 ? (
 							<EmptyCartUI />
 						) : (
@@ -30,18 +24,13 @@ const CartPage = () => {
 							</div>
 						)}
 						{cart.length > 0 && <PeopleAlsoBought />}
-					</motion.div>
+					</div>
 
 					{cart.length > 0 && (
-						<motion.div
-							className='mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full'
-							initial={{ opacity: 0, x: 20 }}
-							animate={{ opacity: 1, x: 0 }}
-							transition={{ duration: 0.5, delay: 0.4 }}
-						>
+						<div className='mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full'>
 							<OrderSummary />
 							<GiftCouponCard />
-						</motion.div>
+						</div>
 					)}
 				</div>
 			</div>
@@ -51,12 +40,7 @@ const CartPage = () => {
 export default CartPage;
 
 const EmptyCartUI = () => (
-	<motion.div
-		className='flex flex-col items-center justify-center space-y-4 py-16'
-		initial={{ opacity: 0, y: 20 }}
-		animate={{ opacity: 1, y: 0 }}
-		transition={{ duration: 0.5 }}
-	>
+	<div className='flex flex-col items-center justify-center space-y-4 py-16'>
 		<ShoppingCart className='h-24 w-24 text-gray-300' />
 		<h3 className='text-2xl font-semibold '>Your cart is empty</h3>
 		<p className='text-gray-400'>Looks like you {"haven't"} added anything to your cart yet.</p>
@@ -66,5 +50,5 @@ const EmptyCartUI = () => (
 		>
 			Start Shopping
 		</Link>
-	</motion.div>
+	</div>
 );
