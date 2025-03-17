72a909cf66646459c94b484e0608594bad3fc90d
Harsh Rajput <101476748+Harshalex@users.noreply.github.com>
2025-03-17 13:12:47 +0530

    Update AdminPage.jsx

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,6 +1,5 @@
 import { BarChart, PlusCircle, ShoppingBasket } from "lucide-react";
 import { useEffect, useState } from "react";
-import { motion } from "framer-motion";
 
 import AnalyticsTab from "../components/AnalyticsTab";
 import CreateProductForm from "../components/CreateProductForm";
@@ -24,14 +23,10 @@ const AdminPage = () => {
 	return (
 		<div className='min-h-screen relative overflow-hidden'>
 			<div className='relative z-10 container mx-auto px-4 py-16'>
-				<motion.h1
-					className='text-4xl font-bold mb-8 text-emerald-400 text-center'
-					initial={{ opacity: 0, y: -20 }}
-					animate={{ opacity: 1, y: 0 }}
-					transition={{ duration: 0.8 }}
-				>
+				{/* Removed the motion animation here */}
+				<h1 className='text-4xl font-bold mb-8 text-emerald-400 text-center'>
 					Admin Dashboard
-				</motion.h1>
+				</h1>
 
 				<div className='flex justify-center mb-8'>
 					{tabs.map((tab) => (
